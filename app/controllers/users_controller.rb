aa3d773a02cec9650fed4e345d08d40acb3b820c
sken100627 <shigujianren01@gmail.com>
2023-09-11 18:51:34 +0900

    ユーザー詳細ページの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
 def show
+  @prototypes = current_user.prototypes
+  @name = current_user.user_name
+  @profile = current_user.user_profile
+  @occupation = current_user.user_occupation
+  @position = current_user.user_position
 end
 
 end
