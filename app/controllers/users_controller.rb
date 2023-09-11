4cc1af64217ee75821a83b74db8938eaafbc1edd
sken100627 <shigujianren01@gmail.com>
2023-09-11 20:12:28 +0900

    ユーザー詳細ページ完成版

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,8 @@
 class UsersController < ApplicationController
 
-def show
-  @prototypes = current_user.prototypes
-  @name = current_user.user_name
-  @profile = current_user.user_profile
-  @occupation = current_user.user_occupation
-  @position = current_user.user_position
-end
-
+  def show
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
+  end
+  
 end
