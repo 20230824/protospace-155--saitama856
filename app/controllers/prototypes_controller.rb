0134f87071ed9617522c8f07ca2ac166d0f72b3a
sumachanabc <bike.cbrider@gmail.com>
2023-09-11 12:44:30 +0900

    バリデーション実装、クリエイトアクション編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,17 +1,28 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_session, except: [:index]
 
   def new
     @prototype = Prototype.new
   end
 
   def create
-    Prototype.create(prototype_params)
-    redirect_to '/'
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def prototype_params
     params.require(:prototype).permit(:image, :prototype_title, :prototype_catch_copy, :prototype_concept).merge(user_id: current_user.id)
+  end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
 end
