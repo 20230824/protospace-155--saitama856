d2c248afcdd2d484dd038b999053ff640283876c
sumachanabc <bike.cbrider@gmail.com>
2023-09-11 17:03:21 +0900

    編集機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_session, except: [:index]
+  before_action :set_prototype, only: [:edit, :update, :show]
+  before_action :move_to_session, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
@@ -18,11 +19,28 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def edit
+    return if @prototype.user == current_user
+    redirect_to root_path
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def prototype_params
     params.require(:prototype).permit(:image, :prototype_title, :prototype_catch_copy,
                                       :prototype_concept).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
   def move_to_session
