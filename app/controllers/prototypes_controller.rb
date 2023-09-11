6dd8bf95d9c5a3064fb10c15faab387041cd68f1
sumachanabc <bike.cbrider@gmail.com>
2023-09-11 11:15:22 +0900

    投稿フォーム作成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,2 +1,12 @@
 class PrototypesController < ApplicationController
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to '/'
+  end
+
 end
