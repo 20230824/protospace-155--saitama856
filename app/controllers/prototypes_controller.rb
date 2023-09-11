5f371b660aa0e77eafc44850a722c8928725dfc0
sumachanabc <bike.cbrider@gmail.com>
2023-09-11 12:52:37 +0900

    ルボコップ

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -15,14 +15,15 @@ class PrototypesController < ApplicationController
   end
 
   private
+
   def prototype_params
-    params.require(:prototype).permit(:image, :prototype_title, :prototype_catch_copy, :prototype_concept).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:image, :prototype_title, :prototype_catch_copy,
+                                      :prototype_concept).merge(user_id: current_user.id)
   end
 
   def move_to_session
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
-  end
+    return if user_signed_in?
 
+    redirect_to new_user_session_path
+  end
 end
