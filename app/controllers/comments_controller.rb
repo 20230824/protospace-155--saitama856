537b28eabfe1818c655329e33a8a2e155f3ce954
20230824 <nike52book@gmail.com>
2023-09-12 15:37:24 +0900

    path改良

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
-    redirect_to "/prototype/#{comment.prototype.id}"  
+    redirect_to prototype_path(comment.prototype)
   end
 
   private
