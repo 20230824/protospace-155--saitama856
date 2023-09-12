f62b8f33793521e67e87d848815fa96c83d1c422
20230824 <nike52book@gmail.com>
2023-09-12 12:22:55 +0900

    コメントモデル作成、アソシエーションの定義

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,8 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-
+  has_many :comments
+  
   with_options presence: true do
     validates :user_id
     validates :prototype_title
